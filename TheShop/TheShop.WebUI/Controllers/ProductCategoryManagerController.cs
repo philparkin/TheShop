d606bedaefffad6bfc700d8aeef7fc8fde8cb9a2
Phil Parkin <phil@pparkin.co.uk>
2019-05-01 09:28:03 +0100

    Unity implemented

--- TheShop/TheShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ TheShop/TheShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheShop.Core.Contracts;
 using TheShop.Core.Models;
 using TheShop.DataAccess.InMemory;
 
@@ -10,11 +11,11 @@ namespace TheShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryManagerController()
+        public ProductCategoryManagerController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = context;
         }
 
         // GET: ProductManager
